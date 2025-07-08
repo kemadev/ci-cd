daea6c348d21ba031ddb46f5e58f284893a671ef
Jérémy Jourdan <github@kema.dev>
2025-07-08 18:38:51 +0200

    up

--- tool/kemutil/cmd/pulumi.go
+++ tool/kemutil/cmd/pulumi.go
@@ -20,18 +20,9 @@ var iacInit = &cobra.Command{
 	Args:  cobra.NoArgs,
 }
 
-var iacUpdate = &cobra.Command{
-	Use:   "up",
-	Short: "Update a Pulumi stack",
-	Long:  `Update pulumi stack in the current directory`,
-	RunE:  pulumi.Update,
-	Args:  cobra.NoArgs,
-}
-
 func init() {
 	rootCmd.AddCommand(iacCmd)
 	iacCmd.AddCommand(iacInit)
-	iacCmd.AddCommand(iacUpdate)
 	iacCmd.PersistentFlags().
 		BoolVarP(&pulumi.DebugEnabled, "debug", "d", false, "Enable debug output for Pulumi commands")
 	iacCmd.PersistentFlags().
